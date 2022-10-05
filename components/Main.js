078bc6be818f0e58903a48de8a967510322fcdfd
Codehash001 <111737745+Codehash001@users.noreply.github.com>
2022-10-05 22:31:36 +0530

    Update Main.js

--- components/Main.js
+++ components/Main.js
@@ -11,12 +11,12 @@ export default function Main() {
         />
         </div>
         
-        <div  className="w-screen h-auto mt-5 flex justify-center my-5 md:my-8">
+        <div  className="cursor-pointer w-screen h-auto mt-5 flex justify-center my-5 md:my-8">
           <div><Link className="cursor-pointer" href='/mint'>
             <div
-            className="font-Righteous p-2 bg-gradient-to-br from-brand-03 to-brand-04
+            className="cursor-pointer font-Righteous p-2 bg-gradient-to-br from-brand-03 to-brand-04
             shadow-md rounded-md md:text-2xl text-[20px] text-black hover:shadow-gray-400/50 hover:text-bold tracking-wide uppercase">
-                  <span className="p-4 md:px-20 lg:px-32">Go To Minting portal </span>
+                  <span className="cursor-pointer p-4 md:px-20 lg:px-32">Go To Minting portal </span>
             </div>
             </Link>
           </div>
